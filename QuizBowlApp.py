08ad0df60d5237825e18046f600952181c19147e
Abigail Pipkin <abigailpipkinttu@gmail.com>
2024-04-07 14:35:42 -0500

    connecting to db

--- QuizBowlApp.py
+++ QuizBowlApp.py
@@ -2,3 +2,13 @@ import tkinter as tk
 from tkinter import ttk
 import sqlite3
 
+class QuizApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Quiz Bowl")
+
+        self.conn = sqlite3.connect('QuizBowl.db')
+        self.cr = self.conn.cursor()
+
+        self.createMain()
+    
